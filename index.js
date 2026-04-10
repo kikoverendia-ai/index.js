d7e2f993e78c4cd899a4b459fe86832b833b46ce
kikoverendia-ai <kikoverendia@gmail.com>
2026-04-10 14:24:23 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 app.use(express.json());
 
 app.get('/webhook', (req, res) => {
-  const VERIFY_TOKEN = "MY_WEBHOOK_VERIFY_TOKEN";
+  const VERIFY_TOKEN = "Chemico@005";
   
   let mode = req.query['hub.mode'];
   let token = req.query['hub.verify_token'];
